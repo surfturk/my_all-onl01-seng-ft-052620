2ccd2c21afc6a7a93cd5d05ef51443e820e7249d
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 16:10:48 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+   index = 0
+   
+   while  index < collection.length
+    if  !yield(collection[index])
+      return false
+    end
+    index += 1
+  end
+  
+  true
 end
